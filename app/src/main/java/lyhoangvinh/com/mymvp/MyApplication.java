3fcf060ace280945728449485342d050c64e41d0
Ly Hoang Vinh <vinh270795@gmail.com>
2018-04-01 19:09:13 +0700

    update SharedPrefs

--- app/src/main/java/lyhoangvinh/com/mymvp/MyApplication.java
+++ app/src/main/java/lyhoangvinh/com/mymvp/MyApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 
+import com.google.gson.Gson;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import lombok.Setter;
@@ -20,15 +22,27 @@ public class MyApplication extends Application {
     @Setter
     protected AppComponent appComponent;
 
+    private Gson mGSon;
+
+    private static MyApplication mSelf;
+
+    public static MyApplication self() {
+        return mSelf;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mGSon = new Gson();
         Realm.init(this);
         RealmConfiguration configuration = new RealmConfiguration.Builder().build();
         Realm.setDefaultConfiguration(configuration);
 
     }
 
+    public Gson getGSon() {
+        return mGSon;
+    }
 
     public static MyApplication get(Activity activity) {
         return (MyApplication)activity.getApplication();
